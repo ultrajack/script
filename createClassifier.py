1d624e6ad7ab448cf76814b075c3a4cddfe70943
jack <jack@ds.jk.to>
2016-05-23 12:17:24 +0900

    initial

---
+++ createClassifier.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""IBM Watson Developer Cloud  Visual Recognition
+"""
+import os
+import sys
+import json
+import requests
+
+from auth import vrauth, vrurl, concat
+url = vrurl%"classifiers"
+
+def namefd(f): #from xx_xx__nnn.zip to name, filename, fd
+  fname = f
+  try: fname = f.split('__')[0]
+  except: pass
+  filepath = './img/%s'%f
+  try: fd = open(filepath, 'rb')
+  except:
+    print "Can't open", f
+    sys.exit(1)
+  return fname, f, fd
+
+def createClassifiers(positive, negative=None): # item is item.zip file
+#def createClassifiers(item): # item is item.zip file
+  #itemname = positive#item
+  ##try: itemname = item.split('.zip')[0]
+  ##except: pass
+  #try: itemname = positive.split('__')[0]
+  #except: pass
+  #payload = {"name":itemname}
+  #files = [positive, "NOTT_MRI.zip"]
+  #filepath = ['./img/%s'%x for x in files]
+  #fds = [open(filepath[x], 'rb') for x in range(2)]
+  #posneg = {'positive_examples': (files[0], fds[0]),
+  #          'negative_examples': (files[1], fds[1]),}
+  #print repr(payload), repr(posneg)
+
+  pos = namefd(positive)
+  neg = namefd(negative)
+  payload = {"name":pos[0]}
+  posneg = {'positive_examples': (pos[1:]), 'negative_examples': (neg[1:]),}
+  print repr(payload), repr(posneg)
+
+  res = requests.post(url, auth=vrauth,
+    data = payload,
+    files = posneg,
+    )
+  if res.status_code == requests.codes.ok:
+    data = json.loads(res.text)
+    print data
+  else:  # error
+    print 'stauts_code: %s(reason: %s)'%(res.status_code, res.reason)
+
+if __name__ == "__main__":
+  try:
+    item = sys.argv[1]
+    pos, neg = item.split(':')
+  except:
+    print "Usage: %s <positive:negative zipfilename under ./img> <neg>"%sys.argv[0]
+    sys.exit(1)
+
+  print pos, neg
+  #createClassifiers(pos)
+  createClassifiers(pos, neg)
+  #createClassifiers(neg, pos)
